d0eed3a2dfaf5e6753020cf4e4f26344659a38d2
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2024-05-03 12:45:08 -0600

    Add files via upload

---
+++ DB.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+class DatabaseHandler:
+    def __init__(self, config):
+        self.config = config
+        self.connection = mysql.connector.connect(**self.config)
+        self.cursor = self.connection.cursor(dictionary=True)
+
+    def query_db(self, query, args=None, one=False):
+        self.cursor.execute(query, args or ())
+        result = self.cursor.fetchall()
+        return (result[0] if result else None) if one else result
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+
